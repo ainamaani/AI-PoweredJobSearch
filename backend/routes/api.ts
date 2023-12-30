6a3b2f87d7a0a2ec644d4eeec0a131343377df2b
Tumuhimbise Godwin <65310616+ushergodwin@users.noreply.github.com>
2023-12-30 22:11:50 +0300

    refactore
    ### Refactoring
    - separated database configuration
    - put all routes in api.ts
    - changed outDir to build
    - deleted the transpired folder
    - excluded build folder

---
+++ backend/routes/api.ts
@@ -0,0 +1,10 @@
+import JobRoutes from "./JobRoutes";
+import ProfileRoutes from "./ProfileRoutes";
+import ApplicationRoutes from "./ApplicationRoutes";
+import { Router } from "express";
+
+export default (app: { use: (arg0: string, arg1: Router) => void; }) => {
+    app.use('/api/jobs', JobRoutes);
+    app.use('/api/profiles', ProfileRoutes);
+    app.use('/api/applications', ApplicationRoutes);
+}
--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
-import dotenv from "dotenv";
-dotenv.config();
-import JobRoutes from "./routes/JobRoutes";
-import ProfileRoutes from "./routes/ProfileRoutes";
-import ApplicationRoutes from "./routes/ApplicationRoutes";
-
+import database from "./config/database";
+import apiRoutes from "./routes/api";
 // set up an express application
 const app = express()
 
@@ -15,33 +10,13 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     console.log(req.body, req.path, req.method)
     next()
 });
 
-// Check if dbURI is defined in the environment variables
-const dbURI = process.env.dbURI;
+//configure routes
+apiRoutes(app);
 
-if (!dbURI) {
-    console.error("Database URI is not set. Please check your environment variables.");
-    process.exit(1); // Exit the process, indicating an error
-}
-
-//routes
-app.use('/api/jobs', JobRoutes);
-app.use('/api/profiles', ProfileRoutes);
-app.use('/api/applications', ApplicationRoutes);
-
-// connect to the database
-mongoose.connect(dbURI)
-    .then(()=>{
-        app.listen(process.env.PORT,()=>{
-            console.log("Connected to the database...");
-        })
-    })
-    .catch((error : any)=>{
-        console.log(error)
-    })
-
-
+// configure database and run the server 
+database(app);
